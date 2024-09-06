4522ba520830813b06dcbecf34c2ee7fcf6b9736
Anujkbalmiki <kumaranuj4588@gmail.com>
2024-09-06 17:50:47 +0530

    Completed

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const backToTop = document.getElementById("backToTop");
+const backToTopButton = document.getElementById("backToTop");
 
 window.onscroll = function () {
-   if (
-     document.body.scrollTop > 100 ||
-     document.documentElement.scrollTop > 100
-   ) {
-     backToTopButton.style.display = "block";
-   } else {
-     backToTopButton.style.display = "none";
-   }
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
 };
+
+backToTopButton.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
